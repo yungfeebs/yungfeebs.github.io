24bf27b207d67d112d2ada10338497500491fed1
Ryan Febus <febusr@augsburg.edu>
2022-11-21 16:28:08 -0600

    bot database was missing a comma

--- botDatabase.js
+++ botDatabase.js
@@ -8,7 +8,7 @@ const questions = [
 
 const answers = [
     ["Students can find class information, grades, and more at this link. <a href='https://moodle.augsburg.edu/moodle2021/login/index.php'> Moodle </a>"],
-    ["You can find more information about residence life at this link. <a href='https://www.augsburg.edu/residencelife/'> Residence Life </a>"]
+    ["You can find more information about residence life at this link. <a href='https://www.augsburg.edu/residencelife/'> Residence Life </a>"],
     ["You are a current student. Can I help you access Residence Life, Student Organizations, Moodle, or Other?"],
     ["two"]
 ,]
