294840e1f9ba20a7e00ca0b731df8a0fe898b941
Ryan Febus <febusr@augsburg.edu>
2022-11-21 16:24:10 -0600

    beginning to add links to augsburg with key words.

--- botDatabase.js
+++ botDatabase.js
@@ -1,10 +1,17 @@
 //botDatabas.js
 const questions = [
-    ["cheese"],
+    ["moodle"],
+    ["residence"],
+    ["current"],
+    ["student"]
 ]
+
 const answers = [
-    ["burger"],
-]
+    ["Students can find class information, grades, and more at this link. <a href='https://moodle.augsburg.edu/moodle2021/login/index.php'> Moodle </a>"],
+    ["You can find more information about residence life at this link. <a href='https://www.augsburg.edu/residencelife/'> Residence Life </a>"]
+    ["You are a current student. Can I help you access Residence Life, Student Organizations, Moodle, or Other?"],
+    ["two"]
+,]
 const tryAgain = [
     ["I do not understand, try again"],
 ]
